a6988d3e5a194a5564df1aee115c0749bf4c20b2
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-11-18 21:46:52 +1300

    Improve behaviour of variable tree searching.

--- src/components/CellMLVariablePickerDialog.vue
+++ src/components/CellMLVariablePickerDialog.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    <InputText 
-      v-model="filterText" 
-      placeholder="Filter components or variables..." 
-      class="w-full mb-2" 
-    />
-    
-    <div class="border-1 border-gray-300 rounded-lg" style="max-height: 40vh; overflow-y: auto;">
+
+    <div class="flex flex-wrap gap-2 mb-2">
+      <Button
+        type="button"
+        icon="pi pi-plus"
+        text
+        label="Expand All"
+        @click="expandAll"
+      />
+      <Button
+        type="button"
+        icon="pi pi-minus"
+        text
+        label="Collapse All"
+        @click="collapseAll"
+      />
+    </div>
+    <div
+      class="border-1 border-gray-300 rounded-lg"
+      style="max-height: 40vh; overflow-y: auto"
+    >
       <Tree
         :value="treeData"
         v-model:selectionKeys="selectedKey"
@@ -17,26 +31,20 @@
         @node-select="onNodeSelect"
         :expandedKeys="expandedKeys"
       />
-      <!--
-        NOTES:
-        - :value="treeData" -> Uses our new computed property
-        - :filter="true" & :filterValue="filterText" -> Enables built-in filtering
-        - @node-select="onNodeSelect" -> Lets us know when a user clicks an item
-      -->
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref, computed, defineProps, defineEmits, watch } from 'vue'
-import InputText from 'primevue/inputtext'
-import Tree from 'primevue/tree' // Import the Tree component
+import Button from 'primevue/button'
+import Tree from 'primevue/tree'
 
 const props = defineProps({
   modelData: {
     type: Object,
-    default: () => ({})
-  }
+    default: () => ({}),
+  },
 })
 
 const emit = defineEmits(['variable-selected'])
@@ -45,41 +53,40 @@ const filterText = ref('')
 const selectedKey = ref(null)
 const expandedKeys = ref({}) // Used to auto-expand on filter
 
-// This computed property transforms your object into the format PrimeVue Tree needs
+// This computed property transforms modelData object into the format PrimeVue Tree needs
 const treeData = computed(() => {
   const tree = []
   let componentIndex = 0
-  
+
   // Iterate over components (e.g., 'main', 'sodium_channel')
   for (const componentName in props.modelData) {
     const componentKey = `${componentIndex}`
     const variableNodes = []
-    
+
     const variables = props.modelData[componentName]
     let variableIndex = 0
-    
+
     // Iterate over variables for that component
     for (const variable of variables) {
-      // *** This is the filter you asked for: only show if 'id' is present ***
       if (variable.id && variable.id.trim() !== '') {
         const variableKey = `${componentIndex}-${variableIndex}`
         variableNodes.push({
           key: variableKey,
           label: variable.name,
           // Store the full variable data here, including component name
-          data: { ...variable, component: componentName }, 
+          data: { ...variable, component: componentName },
           icon: 'pi pi-fw pi-variable', // Custom icon
         })
         variableIndex++
       }
     }
-    
+
     // Only add the component to the tree if it has selectable variables
     if (variableNodes.length > 0) {
       tree.push({
         key: componentKey,
-        label: componentName, // Component name
-        icon: 'pi pi-fw pi-folder', // Custom icon
+        label: componentName,
+        icon: 'pi pi-fw pi-folder',
         children: variableNodes,
       })
       componentIndex++
@@ -95,23 +102,31 @@ const onNodeSelect = (node) => {
   // and not a component (parent)
   if (node.data) {
     emit('variable-selected', node.data)
-    
+
     // Deselect the key so it can be selected again
-    selectedKey.value = null 
+    selectedKey.value = null
   }
+}
+
+const expandAll = () => {
+  const allKeys = {}
+  treeData.value.forEach((node) => {
+    allKeys[node.key] = true
+  })
+  expandedKeys.value = allKeys
+}
+
+const collapseAll = () => {
+  expandedKeys.value = {}
 }
 
 // This 'watch' auto-expands all nodes when the user types
 // in the filter, so they can see the results.
 watch(filterText, (newVal) => {
   if (newVal) {
-    const allKeys = {}
-    treeData.value.forEach(node => {
-      allKeys[node.key] = true
-    })
-    expandedKeys.value = allKeys
+    expandAll()
   } else {
-    expandedKeys.value = {}
+    collapseAll()
   }
 })
 </script>
