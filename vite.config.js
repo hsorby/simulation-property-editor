4ca3646daef3208ea95d439206d51e0e7a62f04f
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-11-25 17:39:56 +1300

    Update vite.config.js to tidy what is included in the built library.

--- vite.config.js
+++ vite.config.js
@@ -3,33 +3,36 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    lib: {
-      // Could also be a dictionary or array of multiple entry points
-      entry: path.resolve(__dirname, 'src/index.js'),
-      name: 'SimPropertyEditor', // The global variable name (for UMD builds)
-      fileName: (format) => `simpropertyeditor.${format}.js`,
-    },
-    rollupOptions: {
-      // Make sure to externalize deps that shouldn't be bundled
-      // into your library.
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps.
-        globals: {
-          vue: 'Vue',
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [vue()],
+    publicDir: command === 'build' ? false : 'public',
+    build: {
+      lib: {
+        // Could also be a dictionary or array of multiple entry points
+        entry: path.resolve(__dirname, 'src/index.js'),
+        name: 'SimPropertyEditor', // The global variable name (for UMD builds)
+        fileName: (format) => `simpropertyeditor.${format}.js`,
+      },
+      rollupOptions: {
+        // Make sure to externalize deps that shouldn't be bundled
+        // into your library.
+        external: ['vue'],
+        output: {
+          // Provide global variables to use in the UMD build
+          // for externalized deps.
+          globals: {
+            vue: 'Vue',
+          },
         },
       },
     },
-  },
-  optimizeDeps: {
-    // Exclude the wasm-based library from pre-bundling.
-    exclude: ["libcellml.js"],
-    esbuildOptions: {
-      target: "es2020",
+    optimizeDeps: {
+      // Exclude the wasm-based library from pre-bundling.
+      exclude: ['libcellml.js'],
+      esbuildOptions: {
+        target: 'es2020',
+      },
     },
-  },
+  }
 })
