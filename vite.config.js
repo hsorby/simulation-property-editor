1d5b544f956a9c2b83c2480742c424341a777ec8
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-11-27 16:52:25 +1300

    Start migrating away from using primeflex to tailwind css.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   return {
-    plugins: [vue()],
+    plugins: [vue(), tailwindcss()],
     publicDir: command === 'build' ? false : 'public',
     build: {
       lib: {
